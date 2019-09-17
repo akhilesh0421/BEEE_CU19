35ce3605f43cd20e838e6464554e3a0b069f7cc4
Akhilesh Sharma <55455350+akhilesh0421@users.noreply.github.com>
2019-09-17 23:48:53 +0530

    Update Exp1 Code.ino

--- Exp1_LED_Flasher/Exp1 Code.ino
+++ Exp1_LED_Flasher/Exp1 Code.ino
@@ -1,1 +1,12 @@
+void setup()
+{
+  pinMode(7, OUTPUT);//Pin number 7 is conneted to LED and it is used for output purpose.
+}
 
+void loop()
+{
+  digitalWrite(7, HIGH);//High is used to supply voltage.
+  delay(1000); // Wait for 1000 millisecond(s); 1 millisecond = 0.001 second(s)
+  digitalWrite(7, LOW);//Low is used to stop voltage supply.
+  delay(1000); // Wait for 1000 millisecond(s)
+}
