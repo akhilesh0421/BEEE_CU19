cf1414356b5053027c294cdd393d6cef1e0764f9
Akhilesh Sharma <55455350+akhilesh0421@users.noreply.github.com>
2019-09-19 23:36:29 +0530

    Update Code.ino

--- Exp2_LED_Chaser/Code.ino
+++ Exp2_LED_Chaser/Code.ino
@@ -1,1 +1,21 @@
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
 
+void loop()
+{
+  int i;
+  for(i=3;i<=7;i++)
+  {
+    if(i==5)
+      continue;
+    digitalWrite(i, HIGH);
+    delay(1000); // Wait for 1000 millisecond(s)
+    digitalWrite(i, LOW);
+    delay(500); // Wait for 1000 millisecond(s)
+  }
+}
