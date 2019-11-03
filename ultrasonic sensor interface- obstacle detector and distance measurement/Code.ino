c756a65d2f8e3ccd3f3fa3f6b3afb7432be5bae0
Akhilesh Sharma <55455350+akhilesh0421@users.noreply.github.com>
2019-11-03 22:52:38 +0530

    Update Code.ino

--- ultrasonic sensor interface- obstacle detector and distance measurement/Code.ino
+++ ultrasonic sensor interface- obstacle detector and distance measurement/Code.ino
@@ -21,7 +21,7 @@ void loop()
   digitalWrite(trig, LOW);
     
   duration= pulseIn(echo, HIGH);
-  distance= duration*0.034/2;
+  distance= duration*0.017;
   
   Serial.print("Distance: ");
   Serial.println(distance);
